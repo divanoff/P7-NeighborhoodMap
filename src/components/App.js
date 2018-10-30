c1b047fbeacd321df4f2f67a883203f72c2d4665
Dimo Ivanov <github@sonicware.org>
2018-10-29 23:08:59 -0400

    added a header component

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import MenuDrawer from './MenuDrawer';
 import MapDisplay from './MapDisplay';
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <MenuDrawer />
         <MapDisplay />
       </div>
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default function() {
+    return (
+        <div className="header" >
+            <h1>Craft beer near you</h1>
+        </div>
+    );
+}
