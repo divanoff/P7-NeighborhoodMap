12b3555609ef0aed3f7e4026b3081e7633c799f6
Dimo Ivanov <github@sonicware.org>
2018-11-12 22:13:59 -0500

    fixed an issue with the Google Maps API key env variable

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {Map, InfoWindow, GoogleApiWrapper} from 'google-maps-react';
 import MapError from './MapError'
 
-const API_KEY = process.env.NODE_ENV;
+const API_KEY = process.env.GMAPS_API;
  
 class MapView extends Component {
     state = {
